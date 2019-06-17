399def8248c47ebe386c6060a8a03d4fce587a1d
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-17 12:11:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("HelloWorld")
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/helloworld')
+def hello():
+    return 'HelloWorld'
+  
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
