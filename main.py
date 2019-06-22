a9eda1c927de26f22da1bc20b95b8d2a316c6b66
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-22 13:23:52 +0200

    homepage added

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return 'HomePage'
+
 @app.route('/helloworld')
 def hello():
-    return 'HelloWorldxx'
+    return 'HelloWorld'
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
