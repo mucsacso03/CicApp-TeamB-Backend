2426cadef10c06acdbb7b3e4dcf682b30fb0438f
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-22 13:21:05 +0200

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/helloworld')
 def hello():
-    return 'HelloWorld'
+    return 'HelloWorldxx'
   
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
