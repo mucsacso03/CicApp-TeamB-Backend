08faf7b87295db06cffd9e9e51b7eff8e91d1e54
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-22 13:26:50 +0200

    register started

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def home():
 @app.route('/helloworld')
 def hello():
     return 'HelloWorld'
-  
+
+
+@app.route('/register')
+def reg():
+    return 'In Progress'
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
