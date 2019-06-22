3fe00300a9ad88638fe1a815a5dbf0246c4cbc11
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-22 22:19:33 +0200

    q

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}')"
 
 @app.route('/')
 def home():
@@ -12,8 +24,9 @@ def hello():
     return 'HelloWorld'
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def reg():
+
     return 'In Progress'
 
 
