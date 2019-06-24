ad584b1a2f7ecad11e58ef236b7ac97b668d4486
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-24 12:38:39 +0200

    packaged

---
+++ CicApp/Routes.py
@@ -0,0 +1,16 @@
+from flask import Flask, url_for, redirect
+from CicApp import app
+
+@app.route('/')
+def home():
+    return 'HomePage'
+
+@app.route('/helloworld')
+def hello():
+    return 'HelloWorld'
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reg():
+
+    return 'In Progress'
--- main.py
+++ main.py
@@ -1,34 +1,4 @@
-import os
-from flask import Flask, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.username}','{self.email}')"
-
-@app.route('/')
-def home():
-    return 'HomePage'
-
-@app.route('/helloworld')
-def hello():
-    return 'HelloWorld'
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def reg():
-
-    return 'In Progress'
-
+from CicApp import app, os
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
