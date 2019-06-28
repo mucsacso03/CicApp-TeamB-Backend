5db1dc529ffcf660af2d5644ca2973a95927ce0d
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-28 18:28:42 +0200

    postman

--- CicApp/Routes.py
+++ CicApp/Routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, url_for, redirect
 from flask_bcrypt import Bcrypt
-from CicApp import app,db
+from CicApp import app,db, request
 from CicApp.Forms import RegForm
 from CicApp.models import User
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
@@ -19,11 +19,18 @@ def hello():
 @app.route('/', methods=['GET', 'POST']) #/register csak gyorsabb igy tesztelni
 def reg():
     form = RegForm()
-    form.username.data = "Jani"
+    '''form.username.data = "Jani"
     form.email.data = "janivok@jano.hu"
     form.password.data = "jani69"
     hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
     user = User(username=form.username.data, email=form.email.data, password=hashed_pw)  #.data kellene
+'''
+    usern = request.form['username']
+    passw = request.form['password']
+    email = request.form['email']
+    hashed_pw = bcrypt.generate_password_hash(passw).decode('utf-8')
+    user = User(username=usern, email=passw, password=hashed_pw)  # .data kellene
+
     #db.session.add(user)
     #db.session.commit()
 
--- CicApp/__init__.py
+++ CicApp/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import bcrypt
 
