9a0ae19a2d835337a3df2ef8386c785947b3ec45
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-28 17:51:02 +0200

    /register

--- CicApp/Routes.py
+++ CicApp/Routes.py
@@ -1,23 +1,30 @@
 from flask import Flask, url_for, redirect
-from CicApp import app,db,bcrypt
+from flask_bcrypt import Bcrypt
+from CicApp import app,db
 from CicApp.Forms import RegForm
 from CicApp.models import User
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 
+bcrypt = Bcrypt(app)
+'''
 @app.route('/')
 def home():
     return 'HomePage'
-
+'''
 @app.route('/helloworld')
 def hello():
     return 'HelloWorld'
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST']) #/register csak gyorsabb igy tesztelni
 def reg():
     form = RegForm()
-    hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-    user = User(username=form.username, email=form.email.data, password=hashed_pw)
-    db.session.add(user)
-    db.session.commit()
+    form.username = "Jani"
+    form.email = "janivok@jano.hu"
+    form.password = "jani69"
+    hashed_pw = bcrypt.generate_password_hash(form.password).decode('utf-8')
+    user = User(username=form.username, email=form.email, password=hashed_pw)  #.data kellene
+    #db.session.add(user)
+    #db.session.commit()
 
     return 'In Progress'
