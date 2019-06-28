c83196d7ddc09c0aa7b6fdf86ffa13105b6f49b6
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-28 17:39:21 +0200

    secret key

--- CicApp/__init__.py
+++ CicApp/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import bcrypt
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
