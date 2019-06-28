45d28549fe92b8aeeefdd41a2dba5305967190dc
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-06-28 17:32:34 +0200

    bcyrpt

--- CicApp/__init__.py
+++ CicApp/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
