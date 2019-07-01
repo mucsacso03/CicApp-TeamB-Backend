cfc6b0e318effd18825fb95ffdc905fc2f3d9227
mucsacso03 <39996411+mucsacso03@users.noreply.github.com>
2019-07-01 15:52:19 +0200

    login

--- CicApp/Routes.py
+++ CicApp/Routes.py
@@ -53,6 +53,6 @@ def login():
         if bcrypt.check_password_hash(passw1, hashed_pw):
             return 'logged in'
     else:
-        string = 'nope' + usern + ' ' + usern1
+        string = 'nope' #+ usern + ' ' + usern1
         return string
 
